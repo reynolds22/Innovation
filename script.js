6c573ae93fd5f0dfe4c0047105c766d3fea0b6ab
reynolds22 <zacharysreynolds1830@gmail.com>
2024-04-29 19:02:47 -0400

    All done with the home page

--- script.js
+++ script.js
@@ -31,3 +31,12 @@ next_btn.addEventListener('click', (e)=>{
     }
     changeSlide();
 })
+
+const scrollContainer = document.querySelectorAll('.products');
+
+for(const item of scrollContainer){
+    item.addEventListener('wheel', (evt)=>{
+       evt.preventDefault();
+       item.scrollLeft += evt.deltaY ;
+    });
+};
